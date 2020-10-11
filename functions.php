96d36906880db2784005df0ae746545e98ebab77
unlikelyobjects <hello@unlikelyobjects.com>
2020-10-11 13:59:15 +0100

    disable fullscreen editor function

--- functions.php
+++ functions.php
@@ -40,6 +40,20 @@ if (function_exists('add_theme_support')) {
 
 }
 
+/*------------------------------------*\
+	Disable fullscreen editor
+\*------------------------------------*/
+
+$user = wp_get_current_user();
+
+if ($user = 'Colin Lewis') {
+    function disable_editor_fullscreen_by_default()
+    {
+        $script = "jQuery( window ).load(function() { const isFullscreenMode = wp.data.select( 'core/edit-post' ).isFeatureActive( 'fullscreenMode' ); if ( isFullscreenMode ) { wp.data.dispatch( 'core/edit-post' ).toggleFeature( 'fullscreenMode' ); } });";
+        wp_add_inline_script('wp-blocks', $script);
+    }
+    add_action('enqueue_block_editor_assets', 'disable_editor_fullscreen_by_default');
+}
 
 
 /*------------------------------------*\
