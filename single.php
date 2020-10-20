cdc1bc230e0a2d9aa70543e849816c6aac177000
unlikelyobjects <hello@unlikelyobjects.com>
2020-10-20 03:47:36 +0100

    correct post_thumbnail call

--- single.php
+++ single.php
@@ -16,21 +16,18 @@
 					<span class="date"><?php the_time('j F Y'); ?></span>
 
 					<!-- post thumbnail -->
-					<?php if (has_post_thumbnail()) : // Check if Thumbnail exists 
-					?>
-
-						<?php foreach ((get_the_category()) as $cat) {
-							// if (!($cat->cat_ID == '3'))
-							// echo '';
-							// elseif (!($cat->cat_ID == '21'))
-							// echo '';
-							if (($cat->cat_ID == '22')) // Don't show the post thumb if this is the Video category.
-								echo '';
+					<?php 
+					if (has_post_thumbnail()) : 
+					
+					// Check if Thumbnail exists 
+						foreach ((get_the_category()) as $cat) {
+							if (($cat->cat_ID == '22')) 
+							// Don't show the post thumb if this is the Video category.
 							else
-								echo '<a href="' . the_permalink() . '" title="' . the_title() . '">' . the_post_thumbnail() . '</a>';
-						} ?>
-
-					<?php endif; ?>
+								the_post_thumbnail();
+							} 
+					 endif; 
+					 ?>
 					<!-- /post thumbnail -->
 
 					<?php the_content(); ?>
