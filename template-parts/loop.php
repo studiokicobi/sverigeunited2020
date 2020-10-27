826d32d6a378e104a94e1ad0ac379eca6af917cb
unlikelyobjects <hello@unlikelyobjects.com>
2020-10-27 16:35:29 +0000

    Link media posts directly to article

--- template-parts/loop.php
+++ template-parts/loop.php
@@ -6,24 +6,44 @@
 
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-					<?php if (has_post_thumbnail()) : // Check if thumbnail exists 
+					<?php
+					if (in_category('media')) {
+
+						// Media category post
+
+						$url = get_field('url');
+
 					?>
-						<a class="nyheter__thumbnail" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-							<?php the_post_thumbnail(array(500, 500));
-							?>
-						</a>
-					<?php endif; ?>
 
-					<h2>
-						<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-					</h2>
+						<h2>
+							<a href="<?php echo $url; ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+						</h2>
 
-					<span class="date"><?php the_time('j F Y'); ?></span>
+						<span class="date"><?php the_time('j F Y'); ?></span>
+
+						<?php
+
+					} else {
+
+						// All other category posts
+
+						if (has_post_thumbnail()) : // Check if thumbnail exists 
+						?>
+							<a class="nyheter__thumbnail" href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+								<?php the_post_thumbnail(array(500, 500));
+								?>
+							</a>
+						<?php endif; ?>
+
+						<h2>
+							<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+						</h2>
+
+						<span class="date"><?php the_time('j F Y'); ?></span>
 
 					<?php theme_base_wp_excerpt('theme_base_wp_index'); // Build custom callback length in functions.php 
+					}
 					?>
-
-					<?php edit_post_link(); ?>
 
 				</article>
 			</div>
