8aa654ff4c48add9b1455a56052a32186fb882ef
unlikelyobjects <hello@unlikelyobjects.com>
2020-10-27 21:20:10 +0000

    pagination

--- template-parts/pagination.php
+++ template-parts/pagination.php
@@ -1,5 +1,7 @@
 <!-- pagination -->
 <div class="pagination">
-	<?php theme_base_wp_pagination(); ?>
+	<?php //theme_base_wp_pagination(); 
+	?>
+	<?php numbered_pagination(); ?>
 </div>
 <!-- /pagination -->
