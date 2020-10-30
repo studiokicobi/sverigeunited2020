039a90274adadbd8c1caf354e0c8a635483151cf
unlikelyobjects <hello@unlikelyobjects.com>
2020-10-30 17:37:54 +0000

    get the cat from the query string if the cat name isn't available

--- template-parts/yearly-nav.php
+++ template-parts/yearly-nav.php
@@ -8,8 +8,30 @@
         $slug = $uriSegments[2];
         // Get the category from the slug
         $cat = get_category_by_slug($slug);
+
+        // Get the current URL
+        $currentUrl = 'http://' . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
+        // Get the query from the retrieved value
+        $query_cat = parse_url($currentUrl, PHP_URL_QUERY);
+
+        // Sort the categories 
+        if ($query_cat == 'cat=21') {
+            $cat_alt = '21';
+        } elseif ($query_cat == 'cat=22') {
+            $cat_alt = '22';
+        } elseif ($query_cat == 'cat=10') {
+            $cat_alt = '10';
+        } else {
+            // nada
+        }
+
         // $catID is the ID we need for the archive array below
-        $catID = $cat->term_id;
+        if ($cat) {
+            $catID = $cat->term_id;
+        } else {
+            // And $cat_alt is the category pulled from the query string – in the cases where it's not named in the second segment.
+            $catID = $cat_alt;
+        }
 
         // The cat arg depends on the "Add category to archive" code found in functions.php
         $args = array(
