a2352ce9db398fa55172ea2aecad3bcab10bc620
Colin Lewis <hello@colinlewis.se>
2023-08-30 11:21:12 +0100

    bg image

--- blocks/omslag.php
+++ blocks/omslag.php
@@ -46,18 +46,7 @@ if (!empty($block['align'])) {
 			$link_target = $lank['target'] ? $lank['target'] : '_self';
 			?>
 
-			<!-- <div class="omslag" style="background-image: url('<?php //the_sub_field('bakgrundsbild'); 
-																	?>');"> -->
-
-			<div class="omslag" style="background-image: url('
-			<?php
-			// Get the background image
-			$bgimage = the_sub_field('bakgrundsbild');
-			$size = 'medium';
-			if ($bgimage) {
-				echo wp_get_attachment_image($bgimage, $size);
-			} ?>
-			');">
+			<div class="omslag" style="background-image: url('<?php the_sub_field('bakgrundsbild'); ?>');">
 
 				<?php if ($lank) : ?>
 					<a class="omslag__img-link" href="<?php echo esc_url($lank['url']); ?>" target="<?php echo esc_attr($link_target); ?>">
